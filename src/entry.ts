1be32049cc35997fdfa2c006117b99163b4fc8e2
georges.michel <georges@reversense.com>
2025-10-04 21:55:27 +0200

    Export Metadata and related types.

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,9 @@
 import { Architecture } from "./Architecture.js";
+import {DataOperation, Metadata, MetadataTopic, MetadataType } from "./Metadata.js";
 import { NodeInternalType, NodeInternalTypeName } from "./NodeInternalType.js";
 import { OperatingSystem } from "./OperatingSystem.js";
 import { Validator } from "./Validator.js";
 import { Nullable } from "./common.js";
 
-export { Architecture, NodeInternalType, NodeInternalTypeName, OperatingSystem, Validator, Nullable};
+export { Architecture, NodeInternalType, NodeInternalTypeName, OperatingSystem, Validator, Nullable,
+        Metadata, DataOperation, MetadataType, MetadataTopic };
